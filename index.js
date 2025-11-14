2163c2e866355bc9d91b66480aa4c2c9afc41f83
zahiduislam1 <zahidulislam211072@gmail.com>
2025-11-14 23:29:35 +0600

    joined event done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 3000;
 
@@ -21,9 +21,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("Community-Builders-bd-db");
+    const createEventCollection = db.collection("Create-Event");
+    const joinedEventCollection = db.collection("Joined-Event");
 
-    app.get("/", (req, res) => {
-      res.send("Server is running fine");
+    app.get("/create-event", async (req, res) => {
+      const result = await createEventCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/create-event/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await createEventCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+    app.get("/manage-event", async (req, res) => {
+      const email = req.query.email;
+      const result = await createEventCollection
+        .find({ created_by: email })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/create-event", async (req, res) => {
+      const data = req.body;
+      const result = await createEventCollection.insertOne(data);
+      res.send(result);
+    });
+    app.post("/joined-event", async (req, res) => {
+      const data = req.body;
+
+      const result = await joinedEventCollection.insertOne(data);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
@@ -33,7 +64,6 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
